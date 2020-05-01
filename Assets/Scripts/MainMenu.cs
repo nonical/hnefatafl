77395ae325709248c0d27b9baa7f3fe8a9275557
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-01 22:14:03 +0200

    Fix styling

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    public void playGame()
-    {
+public class MainMenu : MonoBehaviour {
+    public void playGame() {
         SceneManager.LoadScene("SampleScene");
     }
 
-    public void quitGame()
-    {
+    public void quitGame() {
         Application.Quit();
     }
 }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,58 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class PauseMenu : MonoBehaviour
-{
+public class PauseMenu : MonoBehaviour {
+    public GameObject pieceManager;
+    public GameObject pauseMenuUI;
     public bool isGamePaused = false;
 
-    public GameObject pieceManager;
-
-    public GameObject pauseMenuUI;
-    // Update is called once per frame
-
-   
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isGamePaused)
-            {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isGamePaused) {
                 Resume();
-            }
-            else
-            {
+            } else {
                 Pause();
             }
         }
     }
 
-    private void toggleInputScript(bool val)
-    {
+    private void toggleInputScript(bool val) {
         pieceManager.GetComponent<InputManagement>().enabled = val;
-
     }
-    public void Resume()
-    {
+
+    public void Resume() {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isGamePaused = false;
         toggleInputScript(true);
     }
 
-    public void Pause()
-    {
+    public void Pause() {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isGamePaused = true;
         toggleInputScript(false);
-        
+
     }
 
-    public void Exit()
-    {
+    public void Exit() {
         toggleInputScript(true);
         Time.timeScale = 1f;
         SceneManager.LoadScene("Menu");
