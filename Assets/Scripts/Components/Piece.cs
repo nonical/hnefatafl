945fec556835a0ac1a82434c851b402f27dc5ff3
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-02 21:13:35 +0200

    Implement physical moving of pieces
    Also, only fire FigureMoved event when piece has finished moving.

---
+++ Assets/Scripts/Components/Piece.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Piece : IndexedObject {
+    [Range(1, 10)]
+    public float speed;
+    private GameObject moveTarget;
+    private Vector3 movePos;
+
+    void Update() {
+        if (moveTarget == null) return;
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, movePos, step);
+
+        // when piece has finished moving fire the FigureMoved event
+        if (Vector3.Distance(transform.position, movePos) < 0.001f) {
+            MovementLogic.FigureMoved(gameObject, GameMemory.GetIndices(moveTarget));
+            moveTarget = null;
+        }
+    }
+
+    public void MovePiece(GameObject target) {
+        moveTarget = target;
+
+        movePos = target.transform.position;
+        movePos.y = transform.position.y;
+    }
+}
