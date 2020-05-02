2174206e906692d44918748ffdade415dd4f9f23
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-03 01:40:10 +0200

    Add animations

--- Assets/Scripts/Components/Piece.cs
+++ Assets/Scripts/Components/Piece.cs
@@ -1,25 +1,49 @@
 ﻿using UnityEngine;
 
+// AnimationState:
+//     0 - idle (loop)
+//     1 - walking (loop)
+//     2 - attack
+
 public class Piece : IndexedObject {
     [Range(1, 10)]
     public float speed;
     private GameObject moveTarget;
     private Vector3 movePos;
+    private Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
 
     void Update() {
         if (moveTarget == null) return;
 
+        animator.SetInteger("AnimationState", 1);
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, movePos, step);
 
         // when piece has finished moving fire the FigureMoved event
         if (Vector3.Distance(transform.position, movePos) < 0.001f) {
+            animator.SetInteger("AnimationState", 0);
             MovementLogic.FigureMoved(gameObject, GameMemory.GetIndices(moveTarget));
             moveTarget = null;
         }
     }
 
+    public void AttackAnimation() {
+        animator.SetInteger("AnimationState", 2);
+    }
+
     public void MovePiece(GameObject target) {
+        var (targetI, targetJ) = GameMemory.GetIndices(target);
+
+        if (targetJ == j) {
+            transform.eulerAngles = targetI < i ? new Vector3(0, 0) : new Vector3(0, 180);
+        } else {
+            transform.eulerAngles = targetJ < j ? new Vector3(0, -90) : new Vector3(0, 90);
+        }
+
         moveTarget = target;
 
         movePos = target.transform.position;
