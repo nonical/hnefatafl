44a340edb0adc72879a64ff1bb308f43cc82c898
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-03 02:57:48 +0200

    Don't allow piece movement when a piece is moving

--- Assets/Scripts/Logic/InputManagement.cs
+++ Assets/Scripts/Logic/InputManagement.cs
@@ -3,8 +3,16 @@
 public class InputManagement : MonoBehaviour {
     private Transform selectedPiece;
     private bool isAttackerTurn = true;
+    private bool pieceMoving = false;
 
-    // Update is called once per frame
+    private void Start() {
+        MovementLogic.FigureMoved += (GameObject _, (int, int) __) => {
+            isAttackerTurn = !isAttackerTurn;
+            pieceMoving = false;
+            selectedPiece = null;
+        };
+    }
+
     void Update() {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -14,8 +22,9 @@ public class InputManagement : MonoBehaviour {
             var selectionRenderer = selection.GetComponent<Renderer>();
 
             if (selectionRenderer != null) {
-
                 if (Input.GetMouseButtonDown(0)) {
+                    if (pieceMoving) return;
+
                     // on figure click
                     if (selection.CompareTag("Selectable") || selection.CompareTag("King")) {
                         if (isAttackerTurn && !selection.name.StartsWith("playerA") || !isAttackerTurn && (selection.name.StartsWith("playerA"))) {
@@ -32,10 +41,9 @@ public class InputManagement : MonoBehaviour {
                     // on tile click
                     if (selection.CompareTag("Highlight") ||
                         ((selection.CompareTag("KingTile") || selection.CompareTag("DeathTile")) && selectedPiece.CompareTag("King"))) {
-
+                        pieceMoving = true;
                         MovementLogic.MovePiece(selectedPiece.gameObject, selection.gameObject);
-                        selectedPiece = null;
-                        isAttackerTurn = !isAttackerTurn;
+
                         DebugHelper.LogFigures();
                         DebugHelper.LogTiles();
                     };
