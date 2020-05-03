b8a39dde32a05d5cdcc5e55e93a2998035535923
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-03 15:02:04 +0200

    Add FacingDirection utility class

---
+++ Assets/Scripts/Utility/FacingDirection.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public static class FacingDirection {
+    public static Vector3 Up { get; } = new Vector3(0, -90);
+    public static Vector3 Down { get; } = new Vector3(0, 90);
+    public static Vector3 Left { get; } = new Vector3(0, 0);
+    public static Vector3 Right { get; } = new Vector3(0, 180);
+}
