1600ac6b39f3927aab5ab31951a37497b71444c4
Harun Sabljakovic <harun_sabljakovic@hotmail.com>
2020-05-04 05:13:49 +0200

    Change event signature, add event handlers

--- Assets/Scripts/Logic/WinConditions.cs
+++ Assets/Scripts/Logic/WinConditions.cs
@@ -3,18 +3,42 @@ using System.Linq;
 using UnityEngine;
 
 public class WinConditions : MonoBehaviour {
-    public static Action<string> AttackersWin;
-    public static Action<string> DefendersWin;
+
+    public GameObject pieceManager;
+    public GameObject gameFinishedUI;
+
+
+    public static Action AttackersWin;
+    public static Action DefendersWin;
 
     void Start() {
         MovementLogic.FigureMoved += CheckHavenTiles;
+        AttackersWin += AttackersWinMessage;
+        DefendersWin += DefendersWinMessage;   
     }
 
     public static void CheckHavenTiles(GameObject _, (int i, int j) indices) {
         var havenTileIndices = new[] { (0, 0), (0, 10), (10, 0), (10, 10) };
 
         if (havenTileIndices.Contains(indices)) {
-            DefendersWin?.Invoke("King reached a haven tile!");
+            DefendersWin?.Invoke();
         }
+    }
+
+    private void toggleInputScript(bool val) {
+        pieceManager.GetComponent<InputManagement>().enabled = val;
+    }
+
+    public void DefendersWinMessage() {//bug here idk why
+        gameFinishedUI.transform.GetChild(0).gameObject.SetActive(true); //display defenders win text
+        gameFinishedUI.SetActive(true);
+        Time.timeScale = 0f;
+        toggleInputScript(false);
+    }
+    public void AttackersWinMessage() { //bug here idk why
+        gameFinishedUI.transform.GetChild(1).gameObject.SetActive(true); //display attackers win text
+        gameFinishedUI.SetActive(true);
+        Time.timeScale = 0f;
+        toggleInputScript(false);
     }
 }
