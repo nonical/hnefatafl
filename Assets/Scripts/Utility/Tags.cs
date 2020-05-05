054cc8e01a63c867f2294f567e6cee0c6485b42c
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-05 23:04:02 +0200

    Add new tags for tiles

--- Assets/Scripts/Utility/Tags.cs
+++ Assets/Scripts/Utility/Tags.cs
@@ -6,11 +6,11 @@
     }
 
     struct TileTags {
-        public static string Accessible { get; } = "AT";
-        public static string SpawnA { get; } = "Spawn_Team_A";
-        public static string SpawnB { get; } = "Spawn_Team_B";
-        public static string King { get; } = "KingTile";
-        public static string Haven { get; } = "DeathTile";
-        public static string Highlight { get; } = "Highlight";
+        public static string Accessible { get; } = "tileAccessible";
+        public static string SpawnA { get; } = "tileSpawnA";
+        public static string SpawnB { get; } = "tileSpawnB";
+        public static string King { get; } = "tileKing";
+        public static string Haven { get; } = "tileHaven";
+        public static string Highlight { get; } = "tileHighlight";
     }
 }
