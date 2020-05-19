ee7a15d034d59e244b186f47a510b6b6e7d62a67
Harun Sabljakovic <harun_sabljakovic@hotmail.com>
2020-05-19 02:03:43 +0200

    Add UI camera

---
+++ Assets/Scripts/UI/UICamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class UICamera : MonoBehaviour {
+    [Range(1, 10)]
+    public float rotationSpeed = 5f;
+
+    void Update() {
+        transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * rotationSpeed);
+    }
+}
