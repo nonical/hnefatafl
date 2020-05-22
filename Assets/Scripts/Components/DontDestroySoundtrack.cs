bb8c8637e9f59ebf5f51ff083e15173ad893ee9f
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-23 01:45:30 +0200

    Play soundtrack continuously throughout runtime

---
+++ Assets/Scripts/Components/DontDestroySoundtrack.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DontDestroySoundtrack : MonoBehaviour {
+    static DontDestroySoundtrack instance = null;
+
+    void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
