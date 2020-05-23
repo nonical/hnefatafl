a564f6d1025dc6c8bbb7a154b52f3c8174f988a2
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-23 20:50:36 +0200

    Make candle flame always face camera

---
+++ Assets/Scripts/Components/CameraFacing.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFacing : MonoBehaviour {
+    public Camera CameraUI;
+    public Camera CameraMain;
+
+    private Camera currentCamera;
+
+    private void Start() {
+        currentCamera = CameraUI;
+    }
+
+    private void Update() {
+        if (!CameraUI.isActiveAndEnabled) currentCamera = CameraMain;
+
+        Vector3 v = currentCamera.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(currentCamera.transform.position - v);
+    }
+}
